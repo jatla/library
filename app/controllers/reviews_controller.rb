3133c7856158ced13a031a19f9552327680a66d2
jatla <jatla@Sunithas-MacBook-Air.local>
2014-08-16 12:22:21 -0700

    Fix routing issues with books and reviews (gh #17, #21 and #22)

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,18 +1,19 @@
 class ReviewsController < ApplicationController
   before_action :set_review, only: [:show, :edit, :update, :destroy]
+  before_action :set_book
 
   def index
-    @reviews = Review.all
-    respond_with(@reviews)
+    @reviews = @book.reviews
+    respond_with(@book, @review)
   end
 
   def show
-    respond_with(@review)
+    respond_with(@book, @review)
   end
 
   def new
     @review = Review.new
-    respond_with(@review)
+    respond_with(@book, @review)
   end
 
   def edit
@@ -20,23 +21,29 @@ class ReviewsController < ApplicationController
 
   def create
     @review = Review.new(review_params)
+    @review.user_id = current_user.id
+    @review.book_id = @book.id
     @review.save
-    respond_with(@review)
+    respond_with(@book)
   end
 
   def update
     @review.update(review_params)
-    respond_with(@review)
+    respond_with(@book, @review)
   end
 
   def destroy
     @review.destroy
-    respond_with(@review)
+    respond_with(@book, @review)
   end
 
   private
     def set_review
       @review = Review.find(params[:id])
+    end
+
+    def set_book
+      @book = Book.find(params[:book_id])
     end
 
     def review_params
