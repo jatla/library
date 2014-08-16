11a657f70e806e92e109eddba9f66ff716a27838
jatla <jatla@Sunithas-MacBook-Air.local>
2014-08-16 13:06:42 -0700

    Add average rating calculation for a book

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,12 @@ class Book < ActiveRecord::Base
 	mount_uploader :image, S3Uploader
 
 	has_many :reviews
+
+	def avg_rating
+		if !self.reviews.empty?
+			self.reviews.collect{ |r| r.rating }.reduce(:+) / self.reviews.size
+		else
+			" - "
+		end
+	end
 end
