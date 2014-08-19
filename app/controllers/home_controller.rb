642aa51d9412dd77eddc2743f65d4fac933835fc
jatla <jatla@Sunithas-MacBook-Air.local>
2014-08-18 19:39:18 -0700

    Update the application to display flash messages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+  	flash[:notice] = "Please sign in with your Google account to access all features of this website!"
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -27,4 +27,19 @@ module ApplicationHelper
 			false
 		end
 	end
+
+	def bootstrap_class_for flash_type
+    case flash_type
+      when :success
+        "alert-success"
+      when :error
+        "alert-error"
+      when :alert
+        "alert-block"
+      when :notice
+        "alert-info"
+      else
+        flash_type.to_s
+    end
+  end
 end
